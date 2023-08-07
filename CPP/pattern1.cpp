4831853062897cc80ddc813c80daa0692130a3f5
Adithyan T <adithsreedhar628@gmail.com>
2023-08-07 17:16:20 +0530

    updated

---
+++ CPP/pattern1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,j,n=5;
+    for(i=0;i<n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
